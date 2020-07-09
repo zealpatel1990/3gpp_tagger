c55e0817f171346bd801eb69b5c61f8bcac56b9d
charantej224 <charantej.career@gmail.com>
2020-07-09 10:50:20 -0500

    full code

--- apps/3gg_processor_app.py
+++ apps/3gg_processor_app.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     Domain based Lemmatization
     Note: ## based comments are code and has to be removed appropriately with suitable environments. 
     '''
-    ## download_settings()
+    download_settings()
     pre_processed_file = pre_process_file('configs/input_data.txt')
     # Tag the files using the rules set.
     auto_tag_processor = AutoTagProcessor(pre_processed_file, '3ggpp')
     text_file_path, ann_file_path = auto_tag_processor.tag_words()
     # Using REST call to upload the data into Server.
-    ## doc_service = DocumentService()
-    ## doc_service.push_annotated_verbatim_text(text_file_path, ann_file_path)
+    doc_service = DocumentService()
+    doc_service.push_annotated_verbatim_text(text_file_path, ann_file_path)
